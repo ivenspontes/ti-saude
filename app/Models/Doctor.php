0d283448ec53e4590c3bac2d95a9407d2df82cba
ivens pontes <ivenscardoso@hotmail.com>
2022-09-12 01:59:32 -0300

    fixes and refactor Doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,22 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Doctor extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'crm',
         'specialty_id',
     ];
 
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
     protected $with = [
         'specialty',
     ];
 
-    public function specialty()
+    /**
+     * Get the specialty that owns the Doctor
+     *
+     * @return BelongsTo
+     */
+    public function specialty(): BelongsTo
     {
         return $this->belongsTo(Specialty::class);
     }
--- database/migrations/2022_09_11_211352_create_doctors_table.php
+++ database/migrations/2022_09_11_211352_create_doctors_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('crm');
-
             $table->unsignedBigInteger('specialty_id');
-            $table->foreign('specialty_id')->references('id')->on('specialties');
-
             $table->timestamps();
+
+            $table->foreign('specialty_id')->references('id')->on('specialties');
         });
     }
 
