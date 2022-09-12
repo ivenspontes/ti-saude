398ab0937f1ba458e33af85cf498e9ddc224b8bb
ivens pontes <ivenscardoso@hotmail.com>
2022-09-12 01:41:25 -0300

    fixes and refactor Procedure

--- app/Models/Procedure.php
+++ app/Models/Procedure.php
@@ -9,6 +9,11 @@ class Procedure extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'value',
